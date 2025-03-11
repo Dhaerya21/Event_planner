9edecee6a4991413b8bb6fdced4013e401372285
Dhaerya21 <dhaeryakhanna01@gmail.com>
2025-03-12 00:11:27 +0530

    Basic Outline of the program

---
+++ Event_Planner.py
@@ -0,0 +1,22 @@
+import os
+import datetime
+import time
+import json
+
+def Add_Event():
+    pass
+
+def View_Events():
+    pass
+
+def Update_Event():  
+    pass
+
+def Reminder():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
